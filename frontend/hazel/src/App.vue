46c1edc88f444f66a87379bdecc711b8c3b28c1b
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-06-03 19:29:33 +0100

    "succesful api call"

---
+++ frontend/hazel/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+   <Fetcher />
+  </v-app>
+</template>
+
+<script>
+import Fetcher from "./components/Fetcher.vue"
+
+export default {
+  name: 'App',
+
+  components: {
+    Fetcher
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/hazel/src/components/Fetcher.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+
+
+export default {
+  name: "Fetcher",
+
+
+  created(){
+    this.getTodoLists();
+  },
+
+  methods:{
+    async getTodoLists(){
+      let todoListsApiCall = await Vue.axios.get(
+        'http://localhost:8000/api/todolists/'
+      )
+    
+      console.log(todoListsApiCall)
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+</style>
