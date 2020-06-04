1cd78549469b2a7dfd61aee14c6e5309fec5ca0d
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-06-04 05:40:21 +0100

    got cover flow animation working

--- frontend/hazel/src/App.vue
+++ frontend/hazel/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
+  <ListView />
    <Fetcher />
   </v-app>
 </template>
 
 <script>
-import Fetcher from "./components/Fetcher.vue"
+import Fetcher from "./components/Fetcher.vue";
+import ListView from "./components/ListView.vue";
 
 export default {
   name: 'App',
 
   components: {
-    Fetcher
+    Fetcher,
+    ListView
   },
 
   data: () => ({
--- frontend/hazel/src/main.js
+++ frontend/hazel/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 
+
 Vue.config.productionTip = false
 
 new Vue({
